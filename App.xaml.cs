d60560e763870876c0d4eca64829cd964eff0ef4
MoldoveanuAlex <115549146+MoldoveanuAlex@users.noreply.github.com>
2022-12-04 15:42:08 +0200

    ShoppingLists implementation

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
 ﻿namespace Moldoveanu_Alex_Lab7;
+using System;
+using Moldoveanu_Alex_Lab7.Data;
+using System.IO;
 
 public partial class App : Application
 {
-	public App()
+	static ShoppingListDatabase database;
+
+    public static ShoppingListDatabase Database 
+	{ 
+		get 
+		{
+			if (database == null) 
+			{
+				database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+			} 
+			return database; 
+		} 
+	}
+
+    public App()
 	{
 		InitializeComponent();
 
